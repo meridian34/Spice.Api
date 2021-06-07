8b1d1b570c9df35025e3927ff894305b506426cb
meridian34 <sobol.andrey.25@gmail.com>
2021-06-07 23:52:17 +0300

    Added service (data provider)

---
+++ Spice.Api/Services/SpiceService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Api.Services
+{
+    public class SpiceService
+    {
+        private readonly List<Models.Spice> _spices;
+        public SpiceService()
+        {
+            _spices = new List<Models.Spice>();
+        }
+    }
+}
