59518f81bec26c69b74722adf8b43a28f827ab44
meridian34 <sobol.andrey.25@gmail.com>
2021-06-07 20:50:44 +0300

    added spice controller

---
+++ Spice.Api/Controllers/SpiceController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SpiceController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public SpiceController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                
+            })
+            .ToArray();
+        }
+    }
+}
